f311467781e02ea16fc4a0aaa4662dbec01e0b25
Aditya Ray <aditya90005@gmail.com>
2022-07-13 19:28:28 +0000

    sponsers connected with api

--- src/components/pastSponsers/Sponsers.js
+++ src/components/pastSponsers/Sponsers.js
@@ -1,143 +1,45 @@
 import Carousel from "./Carousel";
 import "./sponsers.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import hackImage from "./hackImage.png";
+import axios from "axios";
 
 const Sponsers = () => {
   let width = window.innerWidth;
+  const [sponsers, setSponsers] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://trekthehill.herokuapp.com/api/sponser/")
+      .then((response) => {
+        setSponsers(response.data);
+      })
+      .catch((error) => {});
+  }, []);
 
   return (
     <div className="Sponser" id="Sponser">
       <h1 className="banner">Past Sponsers</h1>{" "}
       <Carousel show={width < 700 ? 1 : 5}>
-        <div>
-          <div className="sponser-padding">
-            <img
-              src={hackImage}
-              alt="placeholder"
-              style={{ width: "100%", transform: "scale(0.8)" }}
-            />
-          </div>
-          <div className="centered">
-            <strong>
-              <p>Hack Club</p>{" "}
-            </strong>
-          </div>
-        </div>
-
-        <div>
-          <div className="sponser-padding">
-            <img
-              src={hackImage}
-              alt="placeholder"
-              style={{ width: "100%", transform: "scale(0.8)" }}
-            />
-          </div>
-          <div className="centered">
-            <strong>
-              <p>Hack Club</p>{" "}
-            </strong>
-          </div>
-        </div>
-
-        <div>
-          <div className="sponser-padding">
-            <img
-              src={hackImage}
-              alt="placeholder"
-              style={{ width: "100%", transform: "scale(0.8)" }}
-            />
-          </div>
-          <div className="centered">
-            <strong>
-              <p>Hack Club</p>{" "}
-            </strong>
-          </div>
-        </div>
-        <div>
-          <div className="sponser-padding">
-            <img
-              src={hackImage}
-              alt="placeholder"
-              style={{ width: "100%", transform: "scale(0.8)" }}
-            />
-          </div>
-          <div className="centered">
-            <strong>
-              <p>Hack Club</p>{" "}
-            </strong>
-          </div>
-        </div>
-        <div>
-          <div className="sponser-padding">
-            <img
-              src={hackImage}
-              alt="placeholder"
-              style={{ width: "100%", transform: "scale(0.8)" }}
-            />
-          </div>
-          <div className="centered">
-            <strong>
-              <p>Hack Club</p>{" "}
-            </strong>
-          </div>
-        </div>
-        <div>
-          <div className="sponser-padding">
-            <img
-              src={hackImage}
-              alt="placeholder"
-              style={{ width: "100%", transform: "scale(0.8)" }}
-            />
-          </div>
-          <div className="centered">
-            <strong>
-              <p>Hack Club</p>{" "}
-            </strong>
-          </div>
-        </div>
-        <div>
-          <div className="sponser-padding">
-            <img
-              src={hackImage}
-              alt="placeholder"
-              style={{ width: "100%", transform: "scale(0.8)" }}
-            />
-          </div>
-          <div className="centered">
-            <strong>
-              <p>Hack Club</p>{" "}
-            </strong>
-          </div>
-        </div>
-        <div>
-          <div className="sponser-padding">
-            <img
-              src={hackImage}
-              alt="placeholder"
-              style={{ width: "100%", transform: "scale(0.8)" }}
-            />
-          </div>
-          <div className="centered">
-            <strong>
-              <p>Hack Club</p>{" "}
-            </strong>
-          </div>
-        </div>
-        <div>
-          <div className="sponser-padding">
-            <img
-              src={hackImage}
-              alt="placeholder"
-              style={{ width: "100%", transform: "scale(0.8)" }}
-            />
-          </div>
-          <div className="centered">
-            <strong>
-              <p>Hack Club</p>{" "}
-            </strong>
-          </div>
-        </div>
+     
+          {sponsers.map((sponser) => {
+            return (
+              <div>
+                <div className="sponser-padding">
+                  <img
+                    src={sponser.image}
+                    alt={sponser.name}
+                    style={{ width: "100%", transform: "scale(0.8)" }}
+                  />
+                </div>
+                <div className="centered">
+                  <strong>
+                      <p>{sponser.name}</p>{" "}
+                  </strong>
+                </div>
+              </div>
+            );
+          })}
+    
       </Carousel>
     </div>
   );
